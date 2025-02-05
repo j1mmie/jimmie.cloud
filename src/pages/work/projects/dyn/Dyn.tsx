8e0e01e075c32bb47d707759007e24727977f9da
Jimmie Tyrrell <jimmie@subpixel.net>
2025-02-04 17:55:55 -0800

    Update portfolio

--- src/pages/work/projects/dyn/Dyn.tsx
+++ src/pages/work/projects/dyn/Dyn.tsx
@@ -16,12 +16,12 @@ export default function Dyn() {
 
       <p>
         Windows desktop software that automatically updates host records as a
-        network's IP changes.
+        network's IP changes. Local server/client architecture that enables
+        installation as a service or local user.
       </p>
 
       <ProjectSubheader>Role:</ProjectSubheader>
-      <p>Solo developer</p>
-      <p>Created v1 in highschool. Hired full time after college</p>
+      <p>Solo developer (coding and UI/UX design</p>
 
       <ProjectSubheader>Technologies Used:</ProjectSubheader>
       <p>C, Win32 API</p>
@@ -29,7 +29,7 @@ export default function Dyn() {
       <ProjectSubheader>Highlights:</ProjectSubheader>
       <p>
          Praised or its ease of use, low resource usage, and small download
-         size (~400kb). First DDNS client that was able to be run as a service.
+         size (~400kb).
       </p>
 
       <YouTubeIFrame videoId='Np0VeQJwlI0' startTime='4m55s' title='DynDNS Updater Demo' />
